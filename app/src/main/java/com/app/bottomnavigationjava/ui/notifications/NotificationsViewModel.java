721f59f925f098fde5e4f30cbece80e84f90303c
Wyatt Evans <syattevans@gmail.com>
2020-06-27 10:06:14 -0500

    Changed function from private to public.

--- app/src/main/java/com/app/bottomnavigationjava/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/app/bottomnavigationjava/ui/notifications/NotificationsViewModel.java
@@ -13,7 +13,7 @@ public class NotificationsViewModel extends ViewModel {
         mText.setValue("This is notifications fragment");
     }
 
-    public LiveData<String> getText() {
+    LiveData<String> getText() {
         return mText;
     }
 }
