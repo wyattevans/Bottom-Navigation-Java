a14209ba664b32668cadd5d4a8beb2e1358235bc
Wyatt Evans <syattevans@gmail.com>
2020-06-28 10:30:14 -0500

    Converted field to local variable.

--- app/src/main/java/com/app/bottomnavigationjava/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/app/bottomnavigationjava/ui/dashboard/DashboardFragment.java
@@ -18,12 +18,9 @@ import com.app.bottomnavigationjava.ui.home.HomeViewModel;
 public class DashboardFragment extends Fragment {
     private final String simpleTAG = DashboardFragment.class.getSimpleName();
 
-    private DashboardViewModel dashboardViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        DashboardViewModel dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
