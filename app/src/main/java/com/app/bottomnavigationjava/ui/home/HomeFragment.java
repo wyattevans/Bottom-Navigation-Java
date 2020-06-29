69c9f226f5b585fc9329b37dd079d16ff2055eaa
Wyatt Evans <syattevans@gmail.com>
2020-06-29 09:30:36 -0500

    Converted field into local variable.

--- app/src/main/java/com/app/bottomnavigationjava/ui/home/HomeFragment.java
+++ app/src/main/java/com/app/bottomnavigationjava/ui/home/HomeFragment.java
@@ -16,12 +16,9 @@ import com.app.bottomnavigationjava.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
